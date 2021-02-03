ef85be3fe18592a11b8cee97fb30622569ef8e39
Amrit Raj <amritrazzz@gmail.com>
2021-02-04 01:09:46 +0530

    initialized CRA boilerplate with Poll component

---
+++ Dev Project/poll-project/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Poll from './components/Poll.js';
+
+function App() {
+  return (
+    <>
+      <h1>This will be the homepage</h1>
+      <Poll/>
+    </>
+  );
+}
+
+export default App;
